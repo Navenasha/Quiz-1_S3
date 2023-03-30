52681247d223fcdd0e88b084644957dce2e73c28
Navenasha <navenasha05@gmail.com>
2023-03-30 15:55:46 +0800

    exercise

---
+++ quiz1.js
@@ -0,0 +1,57 @@
+let message = "benr"
+//console.log(message, typeof message)
+
+message = 123
+//console.log(message, typeof message)
+
+
+//let str = "BENR"
+//let num = 3.523
+
+//let course = str + num
+//console.log(course, typeof course)
+
+//let str = "1"
+//let num = 1
+
+// let total = parseInt(str) + num
+//console.log(total, typeof total)
+
+//let str = "1.258"
+//let num = 1
+
+//let total = parseFloat(str) + num
+//console.log(total, typeof total)
+
+let data = ["benr", 3532, true]
+
+//console.log(data[0])
+//console.log(data[1])
+//console.log(data[2])
+//console.log(data[3])
+
+//let user ={
+   // name: "Navenasha",
+    //age: 22,
+    //: "FKEKK"
+//}
+//console.log(user.name)
+//console.log(user.age)
+
+let messages = [
+    {
+        name: "usr1",
+        dae: "1/4/2023",
+        msg: "good video",
+        like: 1000
+    },
+    {
+        name: "usr2",
+        dae: "1/2/2023",
+        msg: "good video",
+        like: 3000
+    },
+]
+console.log(messages[1].msg) // >> very good video
+
+
